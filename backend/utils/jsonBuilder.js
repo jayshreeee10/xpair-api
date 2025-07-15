f75cad8ff11d048034a236c5c7b8a80178994c2d
Jayshree <jayshree1032@gmail.com>
2025-07-15 11:00:40 +0530

    Json Builder ifelse

--- backend/utils/jsonBuilder.js
+++ backend/utils/jsonBuilder.js
@@ -57,26 +57,41 @@ function buildJSON(attributes, grouped = {}, attributeMap) {
     grouped[parent].push(attr);
   }
 
+  const typeHandlers = {
+    Object: id => build(id),
+    Array: id => [build(id)],
+    default: () => null
+  };
+
   function build(parentId) {
     const children = grouped[parentId] || [];
     const obj = {};
 
     for (let child of children) {
-      const attrDef = attributeMap[child.attribute_id];
-      const attrName = attrDef.attribute_name;
-      const attrType = attrDef.attribute_type;
+      //   const attrDef = attributeMap[child.attribute_id];
+      //   const attrName = attrDef.attribute_name;
+      //   const attrType = attrDef.attribute_type;
 
-      if (child.is_master_node === 1) {
-        if (attrType === 'Object') {
-          obj[attrName] = build(child.attribute_id);
-        } else if (attrType === 'Array') {
-          obj[attrName] = [build(child.attribute_id)];
-        }
-      } else {
-        // Change this to return actual values if available
-        obj[attrName] = child.attribute_value || null;
-      }
+      //   if (child.is_master_node === 1) {
+      //     if (attrType === 'Object') {
+      //       obj[attrName] = build(child.attribute_id);
+      //     } else if (attrType === 'Array') {
+      //       obj[attrName] = [build(child.attribute_id)];
+      //     }
+      //   } else {
+      //     // Change this to return actual values if available
+      //     obj[attrName] = child.attribute_value || null;
+      //   }
+      // }
+
+      const { attribute_id, is_master_node } = child;
+      const { attribute_name, attribute_type } = attributeMap[attribute_id];
+
+      const handler = typeHandlers[attribute_type] || typeHandlers.default;
+      obj[attribute_name] =
+        is_master_node === 1 ? handler(attribute_id) : null;
     }
+
 
     return obj;
   }
@@ -84,7 +99,7 @@ function buildJSON(attributes, grouped = {}, attributeMap) {
   return build(0);
 }
 
- module.exports = { buildJSON };
+module.exports = { buildJSON };
 
 
 
