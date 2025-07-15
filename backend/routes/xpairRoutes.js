babad5a98de3114836fb25d762e3af9a791f30c1
Aashi-20 <aashi0809.be20@chitkara.edu.in>
2025-07-15 16:42:08 +0530

    Updated code

--- backend/routes/xpairRoutes.js
+++ backend/routes/xpairRoutes.js
@@ -1,17 +1,10 @@
-// const express = require('express');
-// const router = express.Router();
-// const { getXpairData } = require('../controllers/xpairController');
-
-// router.post('/xpair', getXpairData);
-// module.exports = router;
 
 
 const express = require('express');
 const router = express.Router();
 const xpairController = require('../controllers/xpairController');
 
-// POST endpoint for crop sowing data
-// router.post('/xpair', xpairController.getCropSowingData);
+
 
 router.post('/xpair', xpairController.getXpairData);
 
