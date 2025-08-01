8bf297340253d084229853f6fdfdd5c91ae2819f
Jayshree <jayshree1032@gmail.com>
2025-08-01 14:29:34 +0530

    Docker setup

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //Enhanced CORS configuration
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
