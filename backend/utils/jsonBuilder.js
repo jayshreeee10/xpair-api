650d8578a5709acb5fa57fbc90ac746791e8ef96
Aashi-20 <aashi0809.be20@chitkara.edu.in>
2025-08-12 14:02:07 +0530

    any_of placement sequence corrected

--- backend/utils/jsonBuilder.js
+++ backend/utils/jsonBuilder.js
@@ -8,234 +8,157 @@
 //     grouped[parent].push(attr);
 //   }
 
-//   // function buildValidationBase(attrMeta, isMandatory) {
-//   //   return {
-//   //     data_type: attrMeta?.data_type || 'String',
-//   //     is_numeric: attrMeta?.is_numeric ?? false,
-//   //     is_date: attrMeta?.is_date ?? false,
-//   //     is_timestamp: attrMeta?.is_timestamp ?? false,
-//   //     is_mandatory: isMandatory === true || isMandatory === 'TRUE',
-//   //     ...(attrMeta?.min_length !== undefined && { min_length: attrMeta.min_length }),
-//   //     ...(attrMeta?.max_length !== undefined && { max_length: attrMeta.max_length }),
-//   //     ...(attrMeta?.enum?.length && { enum: attrMeta.enum })
-//   //   };
-//   // }
-
-
 //   function buildValidationBase(attrMeta, isMandatory) {
-//   const base = {
-//     data_type: attrMeta?.data_type || 'String',
-//     ...(attrMeta?.is_numeric ? { is_numeric: true } : {}),
-//     ...(attrMeta?.is_date ? { is_date: true } : {}),
-//     ...(attrMeta?.is_timestamp ? { is_timestamp: true } : {}),
-//     ...(isMandatory === true || isMandatory === 'TRUE' ? { is_mandatory: true } : {}),
-//     ...(attrMeta?.min_length !== undefined && attrMeta.min_length !== null ? { min_length: attrMeta.min_length } : {}),
-//     ...(attrMeta?.max_length !== undefined && attrMeta.max_length !== null ? { max_length: attrMeta.max_length } : {}),
-//     ...(attrMeta?.enum?.length ? { enum: attrMeta.enum } : {})
-//   };
-  
-//   return base;
-// }
-
-
-
-//   // function build(parentId) {
-//   //   const children = grouped[parentId] || [];
-//   //   const obj = {};
-
-//   //   for (let child of children) {
-//   //     const {
-//   //       attribute_id,
-//   //       is_master_node,
-//   //       is_master_node_array,
-//   //       is_mandatory
-//   //     } = child;
-
-//   //     const attrMeta = attributeMap[attribute_id];
-//   //     if (!attrMeta) continue;
-
-//   //     const { attribute_name } = attrMeta;
-
-//   //     // Special cases
-//   //     if (attribute_name === 'coordinates') {
-//   //       obj[attribute_name] = withValidation
-//   //         ? [[[buildValidationBase(attrMeta, is_mandatory)]]]
-//   //         : [[[null]]];
-//   //       continue;
-//   //     }
-
-//   //     if (attribute_name === 'crop_image') {
-//   //       obj[attribute_name] = [];
-//   //       continue;
-//   //     }
-
-//   //     if (is_master_node === 1) {
-//   //       if (is_master_node_array === 1) {
-//   //         // Array type master node
-//   //         obj[attribute_name] = withValidation 
-//   //           ? [{
-//   //               ...buildValidationBase(attrMeta, is_mandatory),
-//   //               ...build(attribute_id)
-//   //             }]
-//   //           : [build(attribute_id)];
-//   //       } else {
-//   //         // Object type master node
-//   //         obj[attribute_name] = withValidation
-//   //           ? {
-//   //               ...buildValidationBase(attrMeta, is_mandatory),
-//   //               ...build(attribute_id)
-//   //             }
-//   //           : build(attribute_id);
-//   //       }
-//   //     } else {
-//   //       // Leaf node
-//   //       obj[attribute_name] = withValidation
-//   //         ? buildValidationBase(attrMeta, is_mandatory)
-//   //         : null;
-//   //     }
-//   //   }
-//   //   return obj;
-//   // }
-
-
+//     const base = {
+//       data_type: attrMeta?.data_type || 'String',
+//       ...(attrMeta?.is_numeric ? { is_numeric: true } : {}),
+//       ...(attrMeta?.is_date ? { is_date: true } : {}),
+//       ...(attrMeta?.is_timestamp ? { is_timestamp: true } : {}),
+//       ...(isMandatory === true || isMandatory === 'TRUE' ? { is_mandatory: true } : {}),
+//       ...(attrMeta?.min_length !== undefined && attrMeta.min_length !== null ? { min_length: attrMeta.min_length } : {}),
+//       ...(attrMeta?.max_length !== undefined && attrMeta.max_length !== null ? { max_length: attrMeta.max_length } : {}),
+//       ...(attrMeta?.enum?.length ? { enum: attrMeta.enum } : {})
+//     };
+//     return base;
+//   }
 
 //   function build(parentId) {
-//   const children = grouped[parentId] || [];
-//   const obj = {};
+//     const children = grouped[parentId] || [];
+//     const obj = {};
 
-//   // Group children by any_of_group
-//   const groups = new Map();
-//   const normalAttrs = [];
+//     // Group children by any_of_group
+//     const groups = new Map();
+//     const normalAttrs = [];
 
-//   for (let child of children) {
-//     const groupId = child.any_of_group;
-//     if (groupId === null || groupId === undefined) {
-//       normalAttrs.push(child);
-//     } else {
-//       if (!groups.has(groupId)) groups.set(groupId, []);
-//       groups.get(groupId).push(child);
-//     }
-//   }
-
-//   // Handle normal (non-anyOf) attributes
-//   for (let child of normalAttrs) {
-//     const {
-//       attribute_id,
-//       is_master_node,
-//       is_master_node_array,
-//       is_mandatory
-//     } = child;
-
-//     const attrMeta = attributeMap[attribute_id];
-//     if (!attrMeta) continue;
-
-//     const { attribute_name } = attrMeta;
-
-//     // Special cases
-//     if (attribute_name === 'coordinates') {
-//       obj[attribute_name] = withValidation
-//         ? [[[buildValidationBase(attrMeta, is_mandatory)]]]
-//         : [[[null]]];
-//       continue;
-//     }
-
-//     if (attribute_name === 'crop_image') {
-//       obj[attribute_name] = [];
-//       continue;
-//     }
-
-//     if (is_master_node === 1) {
-//       if (is_master_node_array === 1) {
-//         obj[attribute_name] = withValidation
-//           ? [{
-//               ...buildValidationBase(attrMeta, is_mandatory),
-//               ...build(attribute_id)
-//             }]
-//           : [build(attribute_id)];
+//     for (let child of children) {
+//       const groupId = child.any_of_group;
+//       if (groupId === null || groupId === undefined) {
+//         normalAttrs.push(child);
 //       } else {
-//         obj[attribute_name] = withValidation
-//           ? {
-//               ...buildValidationBase(attrMeta, is_mandatory),
-//               ...build(attribute_id)
-//             }
-//           : build(attribute_id);
+//         if (!groups.has(groupId)) groups.set(groupId, []);
+//         groups.get(groupId).push(child);
 //       }
-//     } else {
-//       obj[attribute_name] = withValidation
-//         ? buildValidationBase(attrMeta, is_mandatory)
-//         : null;
 //     }
-//   }
 
-//   // Handle anyOf groups
-//   for (let [groupId, groupAttrs] of groups.entries()) {
-//     const anyOfArray = [];
-
-//     for (let attr of groupAttrs) {
+//     // Handle normal (non-anyOf) attributes
+//     for (let child of normalAttrs) {
 //       const {
 //         attribute_id,
 //         is_master_node,
 //         is_master_node_array,
 //         is_mandatory
-//       } = attr;
+//       } = child;
 
 //       const attrMeta = attributeMap[attribute_id];
 //       if (!attrMeta) continue;
 
 //       const { attribute_name } = attrMeta;
 
+//       // Special cases
+//       if (attribute_name === 'coordinates') {
+//         obj[attribute_name] = withValidation
+//           ? [[[buildValidationBase(attrMeta, is_mandatory)]]]
+//           : [[[null]]];
+//         continue;
+//       }
+
+//       if (attribute_name === 'crop_image') {
+//         obj[attribute_name] = [];
+//         continue;
+//       }
+
 //       if (is_master_node === 1) {
 //         if (is_master_node_array === 1) {
-//           anyOfArray.push({
-//             [attribute_name]: withValidation
-//               ? [{
-//                   ...buildValidationBase(attrMeta, is_mandatory),
-//                   ...build(attribute_id)
-//                 }]
-//               : [build(attribute_id)]
-//           });
+//           obj[attribute_name] = withValidation
+//             ? [{
+//                 ...buildValidationBase(attrMeta, is_mandatory),
+//                 ...build(attribute_id)
+//               }]
+//             : [build(attribute_id)];
 //         } else {
-//           anyOfArray.push({
-//             [attribute_name]: withValidation
-//               ? {
-//                   ...buildValidationBase(attrMeta, is_mandatory),
-//                   ...build(attribute_id)
-//                 }
-//               : build(attribute_id)
-//           });
+//           obj[attribute_name] = withValidation
+//             ? {
+//                 ...buildValidationBase(attrMeta, is_mandatory),
+//                 ...build(attribute_id)
+//               }
+//             : build(attribute_id);
 //         }
 //       } else {
-//         anyOfArray.push({
-//           [attribute_name]: withValidation
-//             ? buildValidationBase(attrMeta, is_mandatory)
-//             : null
-//         });
+//         obj[attribute_name] = withValidation
+//           ? buildValidationBase(attrMeta, is_mandatory)
+//           : null;
 //       }
 //     }
 
-//     // Attach the anyOf group to the output object
-//     obj[`anyOf_${groupId}`] = { anyOf: anyOfArray };
+//     // Handle anyOf groups
+//     if (groups.size > 0) {
+//       const anyOfArray = [];
+
+//       for (let groupAttrs of groups.values()) {
+//         const groupObj = {};
+
+//         for (let attr of groupAttrs) {
+//           const {
+//             attribute_id,
+//             is_master_node,
+//             is_master_node_array,
+//             is_mandatory
+//           } = attr;
+
+//           const attrMeta = attributeMap[attribute_id];
+//           if (!attrMeta) continue;
+
+//           const { attribute_name } = attrMeta;
+
+//           if (is_master_node === 1) {
+//             if (is_master_node_array === 1) {
+//               groupObj[attribute_name] = withValidation
+//                 ? [{
+//                     ...buildValidationBase(attrMeta, is_mandatory),
+//                     ...build(attribute_id)
+//                   }]
+//                 : [build(attribute_id)];
+//             } else {
+//               groupObj[attribute_name] = withValidation
+//                 ? {
+//                     ...buildValidationBase(attrMeta, is_mandatory),
+//                     ...build(attribute_id)
+//                   }
+//                 : build(attribute_id);
+//             }
+//           } else {
+//             groupObj[attribute_name] = withValidation
+//               ? buildValidationBase(attrMeta, is_mandatory)
+//               : null;
+//           }
+//         }
+
+//         anyOfArray.push(groupObj);
+//       }
+
+//       obj["anyOf"] = anyOfArray;
+//     }
+
+//     return obj;
 //   }
-
-//   return obj;
-// }
-
 
 //   return build(0);
 // }
-
-// module.exports = { buildJSON };
-
 
 
 
 
 function buildJSON(attributes, grouped = {}, attributeMap, withValidation = false) {
-  // Group attributes by their parent ID
+  // Group attributes by their parent ID and sort by placement_sequence
   for (let attr of attributes) {
     const parent = attr.master_node_attribute_id || 0;
     if (!grouped[parent]) grouped[parent] = [];
     grouped[parent].push(attr);
+  }
+
+  // Sort all groups by placement_sequence
+  for (const parentId in grouped) {
+    grouped[parentId].sort((a, b) => a.placement_sequence - b.placement_sequence);
   }
 
   function buildValidationBase(attrMeta, isMandatory) {
@@ -256,21 +179,9 @@ function buildJSON(attributes, grouped = {}, attributeMap, withValidation = fals
     const children = grouped[parentId] || [];
     const obj = {};
 
-    // Group children by any_of_group
-    const groups = new Map();
-    const normalAttrs = [];
-
-    for (let child of children) {
-      const groupId = child.any_of_group;
-      if (groupId === null || groupId === undefined) {
-        normalAttrs.push(child);
-      } else {
-        if (!groups.has(groupId)) groups.set(groupId, []);
-        groups.get(groupId).push(child);
-      }
-    }
-
-    // Handle normal (non-anyOf) attributes
+    // First pass: Handle normal (non-anyOf) attributes while maintaining order
+    const normalAttrs = children.filter(child => child.any_of_group === null || child.any_of_group === undefined);
+    
     for (let child of normalAttrs) {
       const {
         attribute_id,
@@ -320,11 +231,26 @@ function buildJSON(attributes, grouped = {}, attributeMap, withValidation = fals
       }
     }
 
-    // Handle anyOf groups
+    // Second pass: Handle anyOf groups while maintaining their original order
+    const groupAttrs = children.filter(child => child.any_of_group !== null && child.any_of_group !== undefined);
+    const groups = new Map();
+
+    // Group by any_of_group while maintaining order
+    for (let child of groupAttrs) {
+      const groupId = child.any_of_group;
+      if (!groups.has(groupId)) groups.set(groupId, []);
+      groups.get(groupId).push(child);
+    }
+
     if (groups.size > 0) {
       const anyOfArray = [];
 
-      for (let groupAttrs of groups.values()) {
+      // Process groups in order of their first attribute's placement_sequence
+      const sortedGroups = Array.from(groups.entries())
+        .sort(([id1, attrs1], [id2, attrs2]) => 
+          attrs1[0].placement_sequence - attrs2[0].placement_sequence);
+
+      for (let [groupId, groupAttrs] of sortedGroups) {
         const groupObj = {};
 
         for (let attr of groupAttrs) {
@@ -366,7 +292,28 @@ function buildJSON(attributes, grouped = {}, attributeMap, withValidation = fals
         anyOfArray.push(groupObj);
       }
 
-      obj["anyOf"] = anyOfArray;
+      // Find the correct position for the anyOf group based on placement_sequence
+      const firstGroupAttr = groupAttrs[0];
+      let inserted = false;
+
+      // Create a new object to maintain order
+      const orderedObj = {};
+      const keys = Object.keys(obj);
+
+      for (let key of keys) {
+        const attr = normalAttrs.find(a => attributeMap[a.attribute_id]?.attribute_name === key);
+        if (attr && firstGroupAttr.placement_sequence < attr.placement_sequence && !inserted) {
+          orderedObj["anyOf"] = anyOfArray;
+          inserted = true;
+        }
+        orderedObj[key] = obj[key];
+      }
+
+      if (!inserted) {
+        orderedObj["anyOf"] = anyOfArray;
+      }
+
+      return orderedObj;
     }
 
     return obj;
